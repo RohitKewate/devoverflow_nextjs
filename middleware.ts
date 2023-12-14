5160162da5fa9e2dc0e0c20794edbc36be1cf246
RohitKewate <rohit751kewate@gmail.com>
2023-12-14 22:16:52 +0530

    changes middleware

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,19 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-    publicRoutes: [
-        "/",
-        "/question/:id",
-        "/tags",
-        "/tags/:id",
-        "profile/:id",
-        "/community",
-        "/jobs",
-        "/api/webhooks(.*)" // add this line
-      ],
-      ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)"],
-      debug: true,
-  });
+  publicRoutes: [
+    "/api/webhooks(.*)", // add this line
+  ],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/webhook"],
+  debug: true,
+  afterAuth: (req, res, auth) => {
+    // Custom behavior goes here
+    // For example, you might want to redirect to a different page
+    if (!auth) {
+      res.redirect(302, "/sign-in");
+    }
+  },
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
